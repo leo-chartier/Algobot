13b74b75bd2390e648b229422f4c4d60bda1b7ce
leo-chartier <leo.chartier@algosup.com>
2023-12-13 20:37:55 +0100

    Add custom bot class

---
+++ bot.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+
+import os
+
+class Bot(commands.Bot):
+    def __init__(self, intents: discord.Intents) -> None:
+        super().__init__(Bot.__no_prefix, intents=intents)
+
+    async def on_ready(self) -> None:
+        print(f'Logged on as {self.user}!')
+
+    async def setup_hook(self) -> None:
+        await super().setup_hook()
+
+        # Load cogs
+        for filename in os.listdir("cogs"):
+            name, ext = os.path.splitext(filename)
+            if ext != ".py":
+                continue
+            cog_name = "cogs." + name
+            await self.load_extension(cog_name)
+
+        # Sync application commands
+        await self.tree.sync()
+
+    def __no_prefix(self, message: discord.Message) -> str:
+        # Disables the prefix for regular text commands (slash commands only)
+        if message.content.startswith("!"):
+            return "#"
+        return "!"
--- main.py
+++ main.py
@@ -2,19 +2,13 @@
 
 import discord
 
+from bot import Bot
 from utils.config import load as load_config
-
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!')
-
-    async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 config = load_config()
 
-client = MyClient(intents=intents)
+client = Bot(intents)
 client.run(config["token"]) # type: ignore
